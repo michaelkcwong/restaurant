d61e00f17a6cdf8adbcf2c9ef9c9daee4a7cf195
michaelkcwong <wong_michael@rogers.com>
2022-12-13 23:04:25 -0500

    Make button scroll work and start setting up to change image of main poster

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let left = document.getElementsByClassName('bi-caret-left-fill')
-let right = document.getElementsByClassName('bi-caret-right-fill')
+let left = document.getElementsByClassName('bi-caret-left-fill')[0]
+let right = document.getElementsByClassName('bi-caret-right-fill')[0]
 let cards = document.getElementsByClassName('cards')[0];
 
 left.addEventListener('click', () => {
@@ -9,3 +9,5 @@ left.addEventListener('click', () => {
 right.addEventListener('click', () => {
   cards.scrollLeft += 140;
 })
+
+let poster = document.getElementById('poster');
